2dacd7e2b0f5ad4174ec9030cf37894352323bf0
sarah-sajid2002 <you@example.com>
2024-10-03 20:40:04 +0500

    frontend completed

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+PORT = 4000;
+require("./db/connection");
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send(200).json("server start");
+});
+app.listen(PORT, () => {
+  console.log(`server starts at ${PORT}`);
+});
